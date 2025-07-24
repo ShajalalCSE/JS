40233d427424b14a8bb29c99edec2df48763f566
sha jalal <106727351+ShajalalCSE@users.noreply.github.com>
2025-07-24 20:48:39 +0600

    Displaying output using alert,writeln,innerHTML

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function myFunc() {
   document.getElementById("demo").innerHTML =
     "This test is from script.js file";
 }
+
+function myFunc1() {
+  const number1 = 10;
+  const number2 = 20;
+  const sum = number1 + number2;
+  //document.writeln(sum)
+  //alert(sum)
+  document.getElementById("demo").innerHTML =
+    "this sum is from myFunc1. sum = " + sum;
+}
